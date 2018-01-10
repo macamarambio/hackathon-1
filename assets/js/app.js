93cfe6cffeba9a39c147ca3c3d9437ef58b03399
pazautumn <pazcontrerastello@gmail.com>
2018-01-10 10:09:09 -0300

    carrusel, perfiles y logo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,16 @@
 $(document).ready(function () {
     $('#myCarousel').carousel({
-        interval: 10000
-    })
-    $('.fdi-Carousel .item').each(function () {
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-
-        if (next.next().length > 0) {
-            next.next().children(':first-child').clone().appendTo($(this));
-        }
-        else {
-            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-        }
+        interval: 2000
     });
 });
+
+$('.btn-sign').click(function(){
+    	/*if($('#Email').val() === ""){
+    		alert("Ingesa tu Email");
+    	}if($('#Password').val() === ""){
+    		alert("Ingresa tu contraseña");
+    	}else{
+    		console.log("hola");
+    	}*/
+    		window.location.href="UserLoggedIn.html";
+    });
