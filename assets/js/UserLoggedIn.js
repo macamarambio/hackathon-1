2ebde30b0e00e247830911320105b50dc4b88e11
AyitaXD <nadia.morales.leiva@gmail.com>
2018-01-12 02:09:43 -0300

    buscador listo

--- assets/js/UserLoggedIn.js
+++ assets/js/UserLoggedIn.js
@@ -126,17 +126,11 @@ function search(searchText) {
     var year = response.Search[0].Year;
     if (poster !== "N/A") {
       $('.series').append('<img class="imgSeries delete" src="' + poster + '" alt="series">' + '<h4 class="delete">' + title + '</h4>' + '<h5 class="delete">' + year + '</h5>' + '<p class="delete">' + 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' + '</p>');
-      $('#remove').append('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
     };
   });
 };
 
-function remove() {
-  $('#remove').append('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
-};
-
-$('#remove').click(function() {
-  console.log('hola');
+$('#search').click(function() {
   $('.delete').remove();
   $('.glyphicon-remove').remove();
 });
